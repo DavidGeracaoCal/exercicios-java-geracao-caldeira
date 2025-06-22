352e0696cce7bb09d74fc6cd0b514885fd164dd1
DavidGeracaoCal <trawman.rs@gmail.com>
2025-06-22 01:03:42 -0300

    Add files via upload

---
+++ 6 EXERC.java
@@ -0,0 +1,8 @@
+ArrayList<String> horarios = new ArrayList<>();
+horarios.add("08:00");
+horarios.add("10:00");
+horarios.add("12:00");
+horarios.remove(1);
+horarios.add("14:00");
+
+System.out.println(horarios.get(1)); // 12:00
