41ff6097d96e1ffa75221d3e868779f3a3dcf853
DavidGeracaoCal <trawman.rs@gmail.com>
2025-06-22 01:01:37 -0300

    Add files via upload

---
+++ 5 EXERC.java
@@ -0,0 +1,12 @@
+Java
+int notificacoes = 0;
+
+for (int i = 1; i <= 5; i++) {
+    if (i % 2 == 0) {
+        notificacoes += 2;
+    } else {
+        notificacoes++;
+    }
+}
+
+System.out.println(notificacoes); // 8
