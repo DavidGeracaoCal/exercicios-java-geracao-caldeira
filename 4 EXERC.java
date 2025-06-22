91f735f75fbfb28ba5952ec3bce73db400518f24
DavidGeracaoCal <trawman.rs@gmail.com>
2025-06-22 01:00:05 -0300

    Add files via upload

---
+++ 4 EXERC.java
@@ -0,0 +1,10 @@
+Java
+int prioridade = 4;
+int urgencia = 8;
+if (prioridade > 3 && urgencia > 7) {
+System.out.println("Tarefa crítica");
+} else if (prioridade > 3 || urgencia > 7) {
+System.out.println("Tarefa importante");
+} else {
+System.out.println("Tarefa comum"); // Tarefa crítica
+}
